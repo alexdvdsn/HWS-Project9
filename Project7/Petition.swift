9cc4b0e8b8cbce47a1f9d5a205f7e0a306fbb5e7
alexdvdsn <alex@dvdsn.co>
2021-02-09 18:08:47 -0600

    Initial Commit

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Alex Davidson on 2/8/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Alex Davidson on 2/8/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
